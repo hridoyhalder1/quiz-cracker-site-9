1bcd79ae74cd5656fac4351b5e5d478cef1bddc5
hridoyhalder1 <hridoyhalder91@gmail.com>
2022-10-12 14:11:42 +0600

    quiz loaded

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,10 +1,27 @@
 import React from 'react';
+import './Quiz.css';
 
-const Quiz = ({showQuiz}) => {
-    const {id} = showQuiz;
+const Quiz = ({ showQuiz }) => {
+    const { id, question, options } = showQuiz;
     return (
         <div>
-            
+            <div className='quiz-question'>
+                <h5>Quiz:  {question}</h5>
+                <div className='option-grid'>
+                    <form>
+                        <div className='options'><input type="radio" name="" id="" /><p>{options[0]}</p></div>
+                    </form>
+                    <form>
+                        <div className='options'><input type="radio" name="" id="" /><p>{options[1]}</p></div>
+                    </form>
+                    <form>
+                        <div className='options'><input type="radio" name="" id="" /><p>{options[2]}</p></div>
+                    </form>
+                    <form>
+                        <div className='options'><input type="radio" name="" id="" /><p>{options[3]}</p></div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
