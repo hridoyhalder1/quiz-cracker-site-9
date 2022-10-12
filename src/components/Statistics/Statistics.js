6d28a46bf6c21f5d9a10b9b81a79f6a425a6bdfe
hridoyhalder1 <hridoyhalder91@gmail.com>
2022-10-12 11:42:01 +0600

    chart update

--- src/components/Statistics/Statistics.js
+++ src/components/Statistics/Statistics.js
@@ -1,19 +1,33 @@
-import React from 'react';
-import { useLoaderData } from 'react-router-dom';
-import Rechart from '../rechart/Rechart';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import './Statistics.css';
+
+import React, { useEffect, useState } from 'react';
 
 const Statistics = () => {
-    const recharts = useLoaderData().data;
-    console.log(recharts);
+    const [numbers, setNumbers] = useState([]);
+    useEffect(() => {
+        fetch('https://openapi.programming-hero.com/api/quiz')
+            .then(res => res.json())
+            .then(data => setNumbers(data))
+    }, [])
+    console.log(numbers.data);
     return (
-        <div>
-            <h1>Statistics page.</h1>
-            {
-                recharts.map(rechart => <Rechart
-                key={rechart.id}
-                rechart={rechart}
-                ></Rechart>)
-            }
+        <div className='rechart'>
+
+            <LineChart width={500} height={400} data={numbers.data} margin={{
+                top: 5,
+                right: 30,
+                left: 20,
+                bottom: 5,
+            }}>
+                <CartesianGrid strokeDasharray="3 3" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Tooltip />
+                <Legend />
+                <Line type="monotone" dataKey="pv" stroke="#8884d8" activeDot={{ r: 8 }} />
+                <Line type="monotone" dataKey="total" stroke="#82ca9d" />
+            </LineChart>
         </div>
     );
 };
